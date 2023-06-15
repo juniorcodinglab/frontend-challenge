aa1b0a2e75abac42ff03d618ec1cd90afa72ed9b
juniorcesar001 <silvio_cbsj@hotmail.com>
2023-06-15 06:54:21 -0300

    fix: fixing filter responsivity

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,14 +1,30 @@
 "use client"
 
-import styles from './page.module.css'
 import { FilterBar } from '@/components/filter-bar'
 import { ProductsList } from '@/components/products-list'
+import styled from 'styled-components'
+
+const PageWraper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 24px;
+  max-width: 1400px;
+  min-height: 100vh;
+  margin: 0 auto;
+  background-color: var(--color-bg);
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}) {
+      padding: 34px 160px;
+  }
+`
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <PageWraper>
       <FilterBar />
       <ProductsList/>
-    </main>
+    </PageWraper>
   )
 }
